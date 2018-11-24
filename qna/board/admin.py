6e4ac1d277c235b8a57eda40b8109543e11f49a0
kevin kalis <kevinpaulkalis@gmail.com>
2018-11-24 14:06:01 +0800

    Add admin panel

--- qna/board/admin.py
+++ qna/board/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    '''
+    Admin View for Question
+    '''
+    list_display = ('user', 'title', 'body',)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    '''
+    Admin View for Answer
+    '''
+    list_display = ('question', 'user', 'body',)
